fb34ca57a56267398bd2aeda8eb7d803d1d30049
ToldoDM <bertoldo.damiano@gmail.com>
2021-11-25 17:39:24 +0100

    complete list todo

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+todoList = []
+
+def listTodo():
+    for singleTodo in todoList:
+        if singleTodo.completed:
+            print("id:"+singleTodo.id+" - desc:"+singleTodo.text+" - dueDate:"+singleTodo.dueDate+"\n")
+
+
+def createTodo():
+    x = input("Digita la descrizione : ")
+    x = input("Scegli l'operazione da eseguire: ")
+
+
+def main():
+    while(True):
+        print("digita 1-lista todo, 2-creazione todo, 3-completa todo, 99 - termina esecuzione\n")
+        x = input("Scegli l'operazione da eseguire: ")
+        match x:
+            case 1:
+                listTodo()
+            case 2:
+                createTodo()
+            case 3:
+                return "I'm a teapot"
+            case 99:
+                break
+            case _:
+                print("comando non riconosciutos")
 
 
 if __name__ == "__main__":
-    #things
+    main()
